6d78dc8c6dc2945d55fe6646dc4b4c8dda0bac5f
Kobus Viljoen <kobusv@gmail.com>
2017-06-11 22:29:54 +0200

    Split print of data into a separate function

--- app.js
+++ app.js
@@ -6,24 +6,16 @@ const weather = require('./weather/weather');
 const address = encodeURIComponent(argv.address);
 geocode.geocodeAddress(address)
     .then(getWeatherForLocation)
-    .then((resp) => {
-        if (resp.error) {
-            return console.log(resp.error);
-        }
+    .then(printWeatherData);
 
-        console.log(`Temperature: ${ resp.temperature }\u02DAC`);
-        console.log(`Feels like: ${ resp.apparentTemperature }\u02DAC`);
-        console.log(`Wind Speed: ${ resp.windSpeed }km/h`);
-        console.log(`Change for precipitation: ${ resp.precipProbability }%`)
-    });
-
-function getWeatherForLocation(coords) {
-    if (coords.error) {
-        return console.log(coords.error)
+function printWeatherData(resp) {
+    if (resp.error) {
+        return console.log(resp.error);
     }
 
-    console.log(`Weather for: ${ coords.address }`);
-
-    return weather
-        .getWeatherForLocation(coords)
+    console.log(`Temperature: ${ resp.temperature }\u02DAC`);
+    console.log(`Feels like: ${ resp.apparentTemperature }\u02DAC`);
+    console.log(`Wind Speed: ${ resp.windSpeed }km/h`);
+    console.log(`Change for precipitation: ${ resp.precipProbability }%`)
 }
+
